e9e03893e071bf7edabe32d7731ce84bd2f98563
Sahil Ahmed <zehraankhan7860@gmail.com>
2023-07-04 21:34:01 +0530

    updated btn

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-const hamburgerEl = document.querySelector('.hamburger'),
+const hamburgerEl = document.querySelector('.hamburger-btn'),
       menuEl = document.querySelector('.menu');
 
-hamburgerEl.addEventListener('click', () => {
-    hamburgerEl.classList.toggle('active');
-    menuEl.classList.toggle('toggle');
-});
+// hamburgerEl.addEventListener('click', () => {
+//     hamburgerEl.classList.toggle('active');
+//     menuEl.classList.toggle('toggle');
+// });
+
+const activeClass = "active";
+const aliveClass = "toggle";
+// const showClass = "show";
+const overlayEl = document.querySelector(".overlay");
+let isHamburgerOpen = false;
+
+function toggleNav() {
+  hamburgerEl.classList.toggle(activeClass);
+  menuEl.classList.toggle(aliveClass);
+//   overlayEl.classList.toggle(showClass);
+
+  isHamburgerOpen = !isHamburgerOpen;
+
+  if (isHamburgerOpen) {
+    document.body.style.overflow = "hidden"; // Disable scroll
+    window.addEventListener("scroll", disableScroll);
+  } else {
+    document.body.style.overflow = ""; // Enable scroll
+    window.removeEventListener("scroll", disableScroll);
+  }
+}
+
+function disableScroll(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+hamburgerEl.addEventListener("click", toggleNav);
 
 setTimeout(() => {
     const element = document.querySelector('.bot');
